1f22c1c5f3c0af47737010e902f4c89efab11d90
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-12-11 17:18:59 +0330

    sunnery
    created app accounts created serializer file

---
+++ accounts/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+def clean_email(value):
+    if 'admin' in value:
+        raise serializers.ValidationError('admin cant be in email')
+
+class UserRegisterSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    email = serializers.EmailField(required=True,validators=[clean_email])
+    password = serializers.CharField(required=True, write_only=True)
+    password2 = serializers.CharField(required=True, write_only=True)
+
+    def validate_username(self, value):
+        if value == 'admin':
+            raise serializers.ValidationError('username cannot be empty')
+        return value
+    def validate(self,data):
+        if data['password'] != data['password2'] :
+            raise serializers.ValidationError('passwords must match')
+        return data
