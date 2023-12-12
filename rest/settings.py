4660864ceb885955b045e32eb433cb3ce5fecd91
Alidarvishvand <133001519+Alidarvishvand@users.noreply.github.com>
2023-12-12 10:53:18 +0330

    authentication
    Validation of users and current status of users eg
    Registration and login and admin and...
    
    request.user ==> User information is stored in it
    
    request.auth==>User validation is done in it

--- rest/settings.py
+++ rest/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'accounts.apps.AccountsConfig',
-    'rest_framework',   
+    'rest_framework',
+    'rest_framework.authtoken',   
 
 ]
 
@@ -127,3 +128,11 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#new field type=> restframework
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+    ]
+}
